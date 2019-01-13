50c08c75b132875b689b4d2cf3f0a69e2841299e
Grant Woods <grantdwoods@outlook.com>
2019-01-13 14:48:05 -0800

    Copied Stokes' example.
    May change the SQL connection string to a local DB file?

--- SecretSanta/src/SecretSanta.Domain/Services/UserService.cs
+++ SecretSanta/src/SecretSanta.Domain/Services/UserService.cs
@@ -1,12 +1,43 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SecretSanta.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SecretSanta.Domain.Services
 {
-    class UserService
+    public class UserService
     {
-        private DbContext Dbcontext { set; get; }
+        private ApplicationDbContext DbContext { get; }
+        public UserService(ApplicationDbContext context)
+        {
+            DbContext = context;
+        }
+
+        public void UpsertUser(User user)
+        {
+            if (user.Id == default(int))
+            {
+                DbContext.Users.Add(user);
+            }
+            else
+            {
+                DbContext.Users.Update(user);
+            }
+            DbContext.SaveChanges();
+        }
+
+        public User Find(int id)
+        {
+            return DbContext.Users.Find(id);
+        }
+
+        public List<User> FetchAll()
+        {
+            var userTask = DbContext.Users.ToListAsync();
+            userTask.Wait();
+
+            return userTask.Result;
+        }
     }
 }
---
+++ SecretSanta/test/SecretSanta.Domain.Tests/Services/UserServiceTest.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Domain.Models;
+using SecretSanta.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Domain.Tests.Services
+{
+    [TestClass]
+    public class UserServiceTests
+    {
+        private SqliteConnection SqliteConnection { get; set; }
+        private DbContextOptions<ApplicationDbContext> Options { get; set; }
+
+        [TestInitialize]
+        public void OpenConnection()
+        {
+            SqliteConnection = new SqliteConnection("DataSource=:memory:");
+            SqliteConnection.Open();
+
+            Options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlite(SqliteConnection)
+                .Options;
+
+            using (var context = new ApplicationDbContext(Options))
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+        [TestCleanup]
+        public void CloseConnection()
+        {
+            SqliteConnection.Close();
+        }
+        [TestMethod]
+        public void CreateUser()
+        {
+            UserService userService;
+
+            User user = new User
+            {
+                FirstName = "Inigo",
+                LastName = "Montoya"
+            };
+
+            using (var context = new ApplicationDbContext(Options))
+            {
+                userService = new UserService(context);
+
+                userService.UpsertUser(user);
+            }
+
+            using (var context = new ApplicationDbContext(Options))
+            {
+                userService = new UserService(context);
+
+                user = userService.Find(1);
+
+                Assert.AreEqual("Inigo", user.FirstName);
+            }
+        }
+
+        [TestMethod]
+        public void UpdateUser()
+        {
+            UserService userService;
+
+            User user = new User
+            {
+                FirstName = "Inigo",
+                LastName = "Montoya"
+            };
+
+            using (var context = new ApplicationDbContext(Options))
+            {
+                userService = new UserService(context);
+
+                userService.UpsertUser(user);
+            }
+
+            using (var context = new ApplicationDbContext(Options))
+            {
+                userService = new UserService(context);
+
+                user = userService.Find(1);
+                user.FirstName = "Princess";
+                user.LastName = "Buttercup";
+
+                userService.UpsertUser(user);
+            }
+
+            using (var context = new ApplicationDbContext(Options))
+            {
+                userService = new UserService(context);
+
+                user = userService.Find(1);
+
+                Assert.AreEqual("Princess", user.FirstName);
+            }
+        }
+    }
+}
