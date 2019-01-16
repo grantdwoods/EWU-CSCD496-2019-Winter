9da4637876c343e6af867c10fc1dd639f5179c2d
Grant Woods <grantdwoods@outlook.com>
2019-01-16 10:13:44 -0800

    Add user to group
    Based off remove method and assuming both are even close to correct.

--- SecretSanta/src/SecretSanta.Domain/Services/GroupService.cs
+++ SecretSanta/src/SecretSanta.Domain/Services/GroupService.cs
@@ -1,10 +1,45 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SecretSanta.Domain.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SecretSanta.Domain.Services
 {
-    class GroupService
+    public class GroupService
     {
+        private ApplicationDbContext Context { get; set; }
+        public GroupService(ApplicationDbContext context)
+        {
+            this.Context = context;
+        }
+
+        public Group AddGroup(Group group)
+        {
+            Context.Groups.Add(group);
+            Context.SaveChanges();
+
+            return group;
+        }
+
+        public void AddUserToGroup(int groupId, UserGroup userGroup)
+        {
+            var group = Context.Groups
+                .Include(g => g.UserGroups)
+                .SingleOrDefault(g => g.Id == groupId);
+        }
+
+        public void RemoveUserFromGroup(int groupId, int userId)
+        {
+            var group = Context.Groups
+                .Include(g => g.UserGroups)
+                .SingleOrDefault(g => g.Id == groupId);
+
+            var userGroup = group.UserGroups.SingleOrDefault(ug => ug.UserId == userId);
+            group.UserGroups.Remove(userGroup);
+
+            Context.SaveChanges();
+        }
     }
 }
