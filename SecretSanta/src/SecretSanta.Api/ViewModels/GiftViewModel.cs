a18f7f60dac080cc578f874e7a8d988f6e813e17
Grant Woods <grantdwoods@outlook.com>
2019-02-04 16:27:12 -0800

    Database constraints updated

--- SecretSanta/src/SecretSanta.Api/ViewModels/GiftViewModel.cs
+++ SecretSanta/src/SecretSanta.Api/ViewModels/GiftViewModel.cs
@@ -11,7 +11,7 @@ namespace SecretSanta.Api.ViewModels
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public int OrderOfImportance { get; set; }
+        public int? OrderOfImportance { get; set; }
         public string Url { get; set; }
 
         public GiftViewModel()
