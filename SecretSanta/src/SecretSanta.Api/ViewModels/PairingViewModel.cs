e5627d1facac5aaf6955846280c90677fa180d35
Grant Woods <grantdwoods@outlook.com>
2019-02-11 14:48:54 -0800

    Pairing groundwork (Mapper and view model)

---
+++ SecretSanta/src/SecretSanta.Api/ViewModels/PairingViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecretSanta.Api.ViewModels
+{
+    public class PairingViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public int SantaId { get; set; }
+        [Required]
+        public int RecipientId { get; set; }
+    }
+}
