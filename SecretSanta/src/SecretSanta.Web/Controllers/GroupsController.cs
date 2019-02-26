21ca0870976a5b524212e81c8ea526eef6c74c56
Grant Woods <grantdwoods@outlook.com>
2019-02-26 13:13:15 -0800

    Page to add groups and users to groups
    Users to groups not yet implemented, but there.

--- SecretSanta/src/SecretSanta.Web/Controllers/GroupsController.cs
+++ SecretSanta/src/SecretSanta.Web/Controllers/GroupsController.cs
@@ -49,5 +49,70 @@ namespace SecretSanta.Web.Controllers
             }
             return result;
         }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddMembers(int groupId, string groupName)
+        {
+            ViewBag.GroupId = groupId;
+            ViewBag.GroupName = groupName;
+
+            using (var httpClient = ClientFactory.CreateClient("SecretSantaApi"))
+            {
+                var secretSantaClient = new SecretSantaClient(httpClient.BaseAddress.ToString(), httpClient);
+                ViewBag.Users = await secretSantaClient.GetAllUsersAsync();
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddMembers(int groupId, int userId)
+        {
+            IActionResult result = View();
+
+            using (var httpClient = ClientFactory.CreateClient("SecretSantaApi"))
+            {
+                try
+                {
+                    var secretSantaClient = new SecretSantaClient(httpClient.BaseAddress.ToString(), httpClient);
+                    await secretSantaClient.AddUserToGroupAsync(groupId, userId);
+                    result = RedirectToAction(nameof(AddMembers));
+                    ViewBag.SucessMessage = "User added to group!";
+                }
+                catch (SwaggerException se)
+                {
+                    ViewBag.ErrorMessage = se.Message;
+                }
+            }
+            return result;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Add(GroupInputViewModel group)
+        {
+            IActionResult result = View();
+            using (var httpClient = ClientFactory.CreateClient("SecretSantaApi"))
+            {
+                try
+                {
+                    var secretSantaClient = new SecretSantaClient(httpClient.BaseAddress.ToString(), httpClient);
+                    await secretSantaClient.CreateGroupAsync(group);
+                    result = RedirectToAction(nameof(Index));
+                }
+                catch (SwaggerException se)
+                {
+                    ViewBag.ErrorMessage = se.Message;
+                }
+            }
+
+            return result;
+        }
     }
 }
